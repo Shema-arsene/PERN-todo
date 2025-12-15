aff53350570848df78257fd422cd856779fe1b7d
Shema Arsene <aniyonkurushema@gmail.com>
2025-12-15 13:02:16 +0200

    Initialize the frontend app (React.js)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="min-h-screen w-full flex justify-center items-center bg-gray-900">
+      <h1 className="text-3xl text-white font-bold">PERN - Todo App</h1>
+    </main>
+  )
+}
+
+export default App
